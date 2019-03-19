5c64c25139a908edff7dcae8288c2fdc0d5eb014
Júlio César Nunes de Paula <julioccnp@gmail.com>
2019-03-19 20:21:14 -0300

    Ajustando mensagem

--- fenixBack/fenixBack/Controllers/LoginController.cs
+++ fenixBack/fenixBack/Controllers/LoginController.cs
@@ -22,7 +22,7 @@ namespace fenixBack.Controllers
             if (entities.Administrador.FirstOrDefault(adm => adm.Usuario_Id.Equals(usuarioLogado.id)) != null)
                 usuarioLogado.tipoUsuario = "Administrador";
             else
-                usuarioLogado.tipoUsuario = "Usuário";
+                usuarioLogado.tipoUsuario = "Usuario";
 
             return usuarioLogado;
         }
