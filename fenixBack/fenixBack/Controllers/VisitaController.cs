aff8701bab8c790de8dc54fe8fe01504368eff8d
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2019-04-22 18:43:42 -0300

    Método de listar - API

---
+++ fenixBack/fenixBack/Controllers/VisitaController.cs
@@ -0,0 +1,22 @@
+﻿using fenixBack.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace fenixBack.Controllers
+{
+    public class VisitaController: ApiController
+    {
+        [HttpGet]
+        [Route("api/visita/lista")]
+        public List<Visita> listaVisita()
+        {
+            abrigofenixEntities1 entities = new abrigofenixEntities1();
+            var visitas = entities.Visita.ToList();
+            return visitas;
+        }
+    }
+}
