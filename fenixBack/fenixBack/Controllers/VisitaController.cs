aa66bb712193b8d6e951a4261c8379ab0a37286d
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2019-04-24 10:03:30 -0300

    Implementação de pesquisa por data

--- fenixBack/fenixBack/Controllers/VisitaController.cs
+++ fenixBack/fenixBack/Controllers/VisitaController.cs
@@ -18,5 +18,28 @@ namespace fenixBack.Controllers
             var visitas = entities.Visita.ToList();
             return visitas;
         }
+
+        [HttpPost]
+        [Route("api/visita/pesquisar")]
+        public List<Visita> Pesquisar([FromBody] Pesquisa dado)
+        {
+            /*teste*/
+
+            abrigofenixEntities1 entities = new abrigofenixEntities1();
+            DateTime dadoAux;
+          
+            var visitaAux = new Visita();
+            if (DateTime.TryParse(dado.dado, out dadoAux))
+                visitaAux = entities.Visita.FirstOrDefault(visita => visita.dataVisita == dadoAux);
+
+            if (visitaAux == null)
+                return entities.Visita.ToList();
+            else
+            {
+                var visitaList = new List<Visita>();
+                visitaList.Add(visitaAux);
+                return visitaList;
+            }
+        }
     }
 }
