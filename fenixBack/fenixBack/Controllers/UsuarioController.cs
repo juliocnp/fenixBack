f28d39ab3eef89ad08c89dc624a0f79d3afdc2b2
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2019-05-05 13:55:12 -0300

    CRUD Usuários

--- fenixBack/fenixBack/Controllers/UsuarioController.cs
+++ fenixBack/fenixBack/Controllers/UsuarioController.cs
@@ -21,7 +21,43 @@ namespace fenixBack.Controllers
       
             }
 
+        [HttpPost]
+        [Route("api/usuario/")]
+        public bool inserir([FromBody] Usuario usuarioInput)
+        {
+            entities.Usuario.Add(usuarioInput);
+            entities.SaveChanges();
+            if (entities.Usuario.FirstOrDefault(usuario => usuario.id == usuarioInput.id) != null)
+                return true;
+            else
+                return false;
+        }
 
+        [HttpPut]
+        [Route("api/usuario/")]
+        public bool atualizar([FromBody] Usuario usuarioInput)
+        {
+            var usuarioAux = entities.Usuario.FirstOrDefault(usuario => usuario.id == usuarioInput.id);
+            if (usuarioAux == null)
+                return false;
+            entities.Entry(usuarioAux).State = System.Data.Entity.EntityState.Modified;
+            entities.Entry(usuarioAux).CurrentValues.SetValues(usuarioInput);
+            entities.SaveChanges();
+            return true;
+        }
+
+        [HttpDelete]
+        [Route("api/usuario/")]
+        public bool remover([FromBody] Usuario usuarioInput)
+        {
+            entities.Usuario.Attach(usuarioInput);
+            entities.Usuario.Remove(usuarioInput);
+            entities.SaveChanges();
+            if (entities.Usuario.FirstOrDefault(usuario => usuario.id == usuarioInput.id) != null)
+                return true;
+            else
+                return false;
+        }
 
         [HttpGet]
         [Route("api/usuario/tipo")]
@@ -31,7 +67,23 @@ namespace fenixBack.Controllers
             return tipoUsuario;
         }
 
-
+        [HttpPost]
+        [Route("api/usuario/GroupItensTipoUsuario")]
+        public List<Usuario> Pesquisar([FromBody] Pesquisa dado)
+        {
+            int dadoAux;
+            var usuarioAux = new List<Usuario>();
+            if (int.TryParse(dado.dado, out dadoAux))
+                usuarioAux = entities.Usuario.Where(usuario => usuario.TipoUsuario.id == dadoAux).ToList();
+            if (usuarioAux == null)
+                return entities.Usuario.ToList();
+            else
+            {
+                var UsuarioList = new List<Usuario>();
+                UsuarioList = usuarioAux;
+                return UsuarioList;
+            }
+        }
 
 
 
