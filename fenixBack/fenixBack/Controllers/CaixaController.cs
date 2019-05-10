129925252c840d8b86a00b6b6b37e478e72f0846
1044855 <1044855@PSG10INFLB20716>
2019-05-10 19:02:28 -0300

    Criação da Caixa

---
+++ fenixBack/fenixBack/Controllers/CaixaController.cs
@@ -0,0 +1,37 @@
+﻿using fenixBack.DataAccess;
+using fenixBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace fenixBack.Controllers
+{
+
+    /// <summary>
+    /// CRUD de Caixa
+    /// GET: Pegar/Listar
+    /// POST: Inserir
+    /// PUT: Atulizar
+    /// </summary>
+
+
+
+
+    public class CaixaController : ApiController
+    {
+
+        abrigofenixEntities1 entities = new abrigofenixEntities1();
+
+        [HttpGet]
+        [Route("api/caixa/")]
+        public List<Caixa> listaTransacoes()
+        {
+            var estoque = entities.Transa.ToList();
+            return estoque;
+        }
+
+    }
+}
---
+++ fenixBack/fenixBack/Models/Caixa.cs
@@ -0,0 +1,24 @@
+﻿namespace fenixBack.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    using Newtonsoft.Json;
+    using System.Linq;
+    using System.Web;
+
+    [Table("Caixa")]
+    public partial class Caixa
+    {
+        public int ID_TRANSACAO { get; set; }
+
+        public DateTime DATA { get; set; }
+
+        public string DESCRICAO { get; set; }
+
+        public decimal VALOR { get; set; }
+    }
+}
